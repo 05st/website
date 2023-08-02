c13ce47ef879d85ff4f1c18d6e0b5b5b7d27d612
05st <stimsina@protonmail.com>
2023-08-01 20:03:35 -0500

    Fix resume link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,8 @@
 export function Resume() {
     return (
         <>
-          <object class="w-screen h-screen" data="/Resume.pdf" type="application/pdf" width="100%" height="100%">
-              <p>Resume <a href="/Resume.pdf">link</a></p>
+          <object class="w-screen h-screen" data="https://raw.githubusercontent.com/05st/website/master/public/Resume.pdf" type="application/pdf" width="100%" height="100%">
+              <p>Resume <a href="https://raw.githubusercontent.com/05st/website/master/public/Resume.pdf">link</a></p>
           </object>
         </>
     );
