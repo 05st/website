1df85b90266950081193cb8a593b756af562d7ff
05st <stimsina@protonmail.com>
2025-05-16 12:38:02 -0400

    Small updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,5 @@
 import Link from '../components/Link.jsx';
 
-const resumeLink = "https://drive.google.com/file/d/1beDuYbCS_u3OVsZrD9Hcbe09Rz6JU8jR/view"
-
 export function About() {
 	return (
 		<article class="w-full">
@@ -13,7 +11,6 @@ export function About() {
 			<div class="flex flex-row gap-2 mt-1">
 				<Link href="https://github.com/05st" target="_blank">github</Link>
 				<Link href="https://www.linkedin.com/in/stimsina/" target="_blank">linkedin</Link>
-				<Link href={resumeLink} target="_blank">resume</Link>
 			</div>
 		</article>
 	);
